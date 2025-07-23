d11c4c53f4569ff87b4a0d80fd923bbb362d9782
freelw <freelw81@qq.com>
2025-07-23 17:09:57 +0800

    update

--- examples/use_session.ts
+++ examples/use_session.ts
@@ -9,10 +9,11 @@ import { Stagehand } from "@browserbasehq/stagehand";
       env: "BROWSERBASE",
       // env : "LOCAL",
       apiKey: process.env.BROWSERBASE_API_KEY,
-      projectId: process.env.BROWSERBASE_PROJECT_ID!
+      projectId: process.env.BROWSERBASE_PROJECT_ID!,
+      browserbaseSessionID:'df4d0629-3903-48a0-aa14-15d3f9ff9ae5',
 	});
 	await stagehand.init();
 	const page = stagehand.page;
-	await page.goto("https://docs.browserbase.com");
+	await page.goto("https://bilibili.com");
 	// await stagehand.close();
 })().catch((error) => console.error(error.message));
